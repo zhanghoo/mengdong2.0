477df0e052fbfe1c6b4373c1d793e9c0ea79c8df
Hoo <zhanghoo@163.com>
2017-12-21 07:52:38 +0800

    Create AppRegisterLogin.vue

---
+++ src/pages/common/AppRegisterLogin.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="register-login"></div>
+</template>
+<script>
+export default {
+  name: 'registerLogin'
+}
+</script>
+<style lang="scss">
+.register-login {
+
+}
+</style>
