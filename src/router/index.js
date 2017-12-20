17561a809db0d8061a116cd548ccddfb50105b88
Hoo <zhanghoo@163.com>
2017-12-21 07:49:24 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import AppIndex from '@/pages/AppIndex'
-import AppFriend from '@/pages/AppFriend'
-import AppShop from '@/pages/AppShop'
-import AppHome from '@/pages/AppHome'
+import AppGuide from '@/pages/common/AppGuide'
+import AppGuide from '@/pages/common/AppLogin'
+import AppGuide from '@/pages/common/AppRegister'
+import AppGuide from '@/pages/common/AppRegisterLogin'
+import AppIndex from '@/pages/index/AppIndex'
+import AppFriend from '@/pages/friend/AppFriend'
+import AppShop from '@/pages/shop/AppShop'
+import AppHome from '@/pages/home/AppHome'
 
 Vue.use(Router)
 
 export default new Router({
   linkActiveClass: 'active',
   routes: [
+    {
+      path: '/guide',
+      name: 'guide',
+      component: AppGuide
+    },
     {
       path: '/index',
       name: 'index',
