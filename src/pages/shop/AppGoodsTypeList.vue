3f53bdfa2255983e80f7f166d7c326f93630c0c8
Hoo <zhanghoo@163.com>
2017-12-21 08:11:58 +0800

    Create AppGoodsTypeList.vue

---
+++ src/pages/shop/AppGoodsTypeList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="goods-type-list"></div>
+</template>
+<script>
+export default {
+  name: 'goodsTypeList'
+}
+</script>
+<style lang="scss">
+.goods-type-list {
+
+}
+</style>
