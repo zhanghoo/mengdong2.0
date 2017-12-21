ec58276b647efb1ed7f6acb26ef34c07b24c0b6e
Hoo <zhanghoo@163.com>
2017-12-21 09:02:44 +0800

    Create TabsMainBlogArticlePanel.vue

---
+++ src/components/TabsMainBlogArticlePanel.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="blog-article-panel"></div>
+</template>
+<script>
+export default {
+  name: 'tabsMainBlogArticlePanel'
+}
+</script>
+<style lang="scss">
+.blog-article-panel {
+
+}
+</style>
