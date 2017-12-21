fe8a1fbd0d9a6ceb781dc1753396f89be4c7d0c9
Hoo <zhanghoo@163.com>
2017-12-21 09:31:55 +0800

    Create TabsGoodsSupertype.vue

---
+++ src/components/TabsGoodsSupertype.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="goods-supertype"></div>
+</template>
+<script>
+export default {
+  name: 'tabsGoodsSupertype'
+}
+</script>
+<style lang="scss">
+.goods-supertype {
+
+}
+</style>
