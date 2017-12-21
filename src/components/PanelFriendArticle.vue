11a0b05ef46d065c739693025400555acac54cef
Hoo <zhanghoo@163.com>
2017-12-21 16:48:21 +0800

    Create PanelFriendArticle.vue

---
+++ src/components/PanelFriendArticle.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="friend-article-panel"></div>
+</template>
+<script>
+export default {
+  name: 'panelFriendArticle'
+}
+</script>
+<style lang="scss">
+.friend-article-panel {
+}
+</style>
