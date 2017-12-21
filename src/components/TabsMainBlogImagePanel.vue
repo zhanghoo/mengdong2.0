e1a59086b035210bd1a18bca07bd8f276dbd48cc
Hoo <zhanghoo@163.com>
2017-12-21 09:08:09 +0800

    Create TabsMainBlogImagePanel.vue

---
+++ src/components/TabsMainBlogImagePanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="blog-image-panel"></div>
+</template>
+<script>
+export default {
+  name: 'tabsMainBlogImagePanel'
+}
+</script>
+<style lang="scss">
+.blog-image-panel {
+}
+</style>
