1e6395633a2e1edf2b6bfca8c7b31a0d84dd0736
Hoo <zhanghoo@163.com>
2017-12-21 09:30:38 +0800

    Create TabsGoodsSubtype.vue

---
+++ src/components/TabsGoodsSubtype.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="goods-subtype"></div>
+</template>
+<script>
+export default {
+  name: 'tabsGoodsSubtype'
+}
+</script>
+<style lang="scss">
+.goods-subtype {
+
+}
+</style>
