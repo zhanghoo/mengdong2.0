7f61e7c493f2410436ffc7e0be22ef60d05e716e
Hoo <zhanghoo@163.com>
2017-12-21 09:20:20 +0800

    Create TabsMainGoodsPickedPanel.vue

---
+++ src/components/TabsMainGoodsPickedPanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="goods-picked"></div>
+</template>
+<script>
+export default {
+  name: 'tabsMainGoodsPickedPanel'
+}
+</script>
+<style lang="scss">
+.goods-picked {
+}
+</style>
