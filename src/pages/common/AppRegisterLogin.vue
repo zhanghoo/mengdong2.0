d795af9851485822948569610e57e6610b06f0cb
Hoo <zhanghoo@163.com>
2017-12-21 09:50:47 +0800

    Update AppRegisterLogin.vue

--- src/pages/common/AppRegisterLogin.vue
+++ src/pages/common/AppRegisterLogin.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="register-login"></div>
+  <div class="app-register-login"></div>
 </template>
 <script>
 export default {
-  name: 'registerLogin'
+  name: 'appRegisterLogin'
 }
 </script>
 <style lang="scss">
-.register-login {
+.app-register-login {
 
 }
 </style>
