0d906fcb035e3dd5f5e5705eda1b04daebac5853
Hoo <zhanghoo@163.com>
2017-12-21 09:22:57 +0800

    Create TabsMainGoodsLimitPanel.vue

---
+++ src/components/TabsMainGoodsLimitPanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="goods-limit"></div>
+</template>
+<script>
+export default {
+  name: 'tabsMainGoodsLimitPanel'
+}
+</script>
+<style lang="scss">
+.goods-limit {
+}
+</style>
