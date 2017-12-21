77bbd913a05373a014b0a97df956c6b79f20eff6
Hoo <zhanghoo@163.com>
2017-12-21 16:49:11 +0800

    Create PanelFriendImage.vue

---
+++ src/components/PanelFriendImage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="friend-image-panel"></div>
+</template>
+<script>
+export default {
+  name: 'panelFriendImage'
+}
+</script>
+<style lang="scss">
+.friend-image-panel {
+}
+</style>
