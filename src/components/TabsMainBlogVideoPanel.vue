e6bac977d4fc4117d26f6067b10ac14e2b782f04
Hoo <zhanghoo@163.com>
2017-12-21 09:08:36 +0800

    Create TabsMainBlogVideoPanel.vue

---
+++ src/components/TabsMainBlogVideoPanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="blog-video-panel"></div>
+</template>
+<script>
+export default {
+  name: 'tabsMainBlogVideoPanel'
+}
+</script>
+<style lang="scss">
+.blog-video-panel {
+}
+</style>
