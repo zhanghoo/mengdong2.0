3ce1aa6f09c225197cece5e6602363cfb4a69d6a
Hoo <zhanghoo@163.com>
2017-12-21 16:49:43 +0800

    Create PanelFriendVideo.vue

---
+++ src/components/PanelFriendVideo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="friend-video-panel"></div>
+</template>
+<script>
+export default {
+  name: 'panelFriendVideo'
+}
+</script>
+<style lang="scss">
+.friend-video-panel {
+}
+</style>
