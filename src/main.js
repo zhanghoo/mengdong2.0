d65eb02b2cb68d0654a1dbe543527cc52f08379c
Hoo <zhanghoo@163.com>
2017-12-22 12:43:47 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,19 @@ import 'normalize.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 // Vue.use(VueAwesomeSwiper, /* { default global options } */)
-Vue.use(VueAwesomeSwiper)
+
+const swiperOptions = {
+  /* eslint-disable */
+  clickable: true,
+  autoplay: false,
+  roundLengths: true, // 防止文字模糊
+  observer: true, // 修改swiper自己或子元素时，自动初始化swiper
+  observeParents: true, // 修改swiper的父元素时，自动初始化swiper
+  mousewheel: true
+  /* eslint-enable */
+}
+
+Vue.use(VueAwesomeSwiper, swiperOptions)
 
 Vue.config.productionTip = false
 
